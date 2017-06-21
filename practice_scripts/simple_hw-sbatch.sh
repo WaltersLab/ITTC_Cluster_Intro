d2bb8c9e9e4a842875555932dfbcbdb34a7611db
James Walters <jwalters@login2.ittc.ku.edu>
2017-06-21 14:00:38 -0500

    added practice scripts for cluster usage

---
+++ practice_scripts/simple_hw-sbatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J HloWorld
+#SBATCH -p intel
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --mem=1GB
+
+sleep 2
+echo "Hello World of clusters"
+sleep 2 
+echo "I am learning how to use the ACF!"
+sleep 2
+echo "Rock Chalk Jayhawk!"
+sleep 2
+echo "Wait, what the heck does that actually mean??"
+#cd nowwhere
+
---
+++ practice_scripts/simple_hw.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Hello World of clusters"
+sleep 2 
+echo "I am learning how to use the ACF!"
+sleep 2
+echo "Rock Chalk Jayhawk!"
+sleep 2
+echo "Wait, what the heck does that actually mean??"
+
